f297bf06b61e3a8530a0b68d076838ca30fe8c38
Marcel Dan <gigi.dan2011@gmail.com>
2020-03-18 15:43:25 +0100

    modificacion tablas sql

--- Database/SQL scripts/tablasmySQL.sql
+++ Database/SQL scripts/tablasmySQL.sql
@@ -1,9 +1,10 @@
 create table redesWifi(
 ssid varchar(250),
 pwr double,
+idComercio int,
 time timestamp,
 idWifi int primary key,
-idComercio int references comercio
+foreign key(idComercio) references comercio(idComercio)
 );
 
 create table comercio(
@@ -16,9 +17,10 @@ idComercio int primary key
 create table ubicacion(
 nombreZona varchar(250),
 horaUbicacion timestamp,
+idWifi int,
 margenError float,
 idUbicacion int primary key,
-idWifi int references redesWifi
+foreign key(idWifi) references redesWifi(idWifi)
 );
 
 create table intolerancia(
@@ -28,37 +30,47 @@ idIntolerancia int primary key
 
 create table ingredientes(
 nombreIngrediente varchar(20),
+idIntolerancia int,
 idIngrediente int primary key,
-idIntolerancia int references intolerancia
+foreign key(idIntolerancia) references intolerancia(idIntolerancia)
 );
 
 create table producto(
 nombreProducto varchar(250),
 codigoBarras varchar(13),
 fabricante varchar(250),
+idIngrediente int,
 telefono varchar(9),
 idProducto int primary key,
-idIngredientes int references ingredientes
+foreign key(idIngrediente) references ingredientes(idIngrediente)
 );
 
 create table usuario(
 idUsuario int primary key,
-idProducto int references producto,
-idIntolerancia int references intolerancia,
-idUbicacion int references ubicacion,
-idComercio int references comercio
+idIntolerancia int,
+idComercio int,
+idProducto int,
+idUbicacion int,
+foreign key(idProducto) references producto(idProducto),
+foreign key(idIntolerancia) references intolerancia(idIntolerancia),
+foreign key(idUbicacion) references ubicacion(idUbicacion),
+foreign key(idComercio) references comercio(idComercio)
 );
 
 create table perfilIntolerancia(
 rangoIntolerancia int,
 idPerfil int primary key,
-idUsuario int references usuario,
-idIntolerancia int references intolerancia
+idIntolerancia int,
+idUsuario int,
+foreign key(idUsuario) references usuario(idUsuario),
+foreign key(idIntolerancia) references intolerancia(idIntolerancia)
 );
 
 create table contador(
 vecesEscaneado int,
+idUsuario int,
+idProducto int,
 idContador int primary key,
-idUsuario int references usuario,
-idProducto int references producto
+foreign key(idUsuario) references usuario(idUsuario),
+foreign key (idProducto) references producto(idProducto)
 );
