8bd17b87c69684856200b47d72e86656da2dc38b
Marcel Dan <gigi.dan2011@gmail.com>
2020-04-06 17:59:27 +0200

    Modificadas unas tablas de la base de datos + nuevo EER

--- Code/NFCVertx/src/main/java/types/usuarioIntolerancias.java
+++ Code/NFCVertx/src/main/java/types/usuarioIntolerancias.java
@@ -5,51 +5,29 @@ import java.util.List;
 public class usuarioIntolerancias {
 	
 	private List<Integer> intolerancias;
-	private Integer idComercio;
+
 	
-	public usuarioIntolerancias() { 
+	public usuarioIntolerancias() {
 		
 	}
-	
 	public usuarioIntolerancias(List<Integer> intolerancias) {
 		super();
 		this.intolerancias = intolerancias;
 	}
-	
-	
-	public usuarioIntolerancias(List<Integer> intolerancias, Integer idComercio) {
-		super();
-		this.intolerancias = intolerancias;
-		this.idComercio = idComercio;
-	}
-	
-	public usuarioIntolerancias(Integer comercio) {
-		super();
-		this.idComercio = comercio;
-	}
-	
-	
 	public List<Integer> getIntolerancias() {
 		return intolerancias;
 	}
 	public void setIntolerancias(List<Integer> intolerancias) {
 		this.intolerancias = intolerancias;
 	}
-	public Integer getIdComercio() {
-		return idComercio;
-	}
-	public void setIdComercio(Integer idComercio) {
-		this.idComercio = idComercio;
-	}
 	@Override
 	public String toString() {
-		return "usuarioIntolerancias [intolerancias=" + intolerancias + ", idComercio=" + idComercio + "]";
+		return "usuarioIntolerancias [intolerancias=" + intolerancias + "]";
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((idComercio == null) ? 0 : idComercio.hashCode());
 		result = prime * result + ((intolerancias == null) ? 0 : intolerancias.hashCode());
 		return result;
 	}
@@ -62,11 +40,6 @@ public class usuarioIntolerancias {
 		if (getClass() != obj.getClass())
 			return false;
 		usuarioIntolerancias other = (usuarioIntolerancias) obj;
-		if (idComercio == null) {
-			if (other.idComercio != null)
-				return false;
-		} else if (!idComercio.equals(other.idComercio))
-			return false;
 		if (intolerancias == null) {
 			if (other.intolerancias != null)
 				return false;
