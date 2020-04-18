6f174d8fe9b23dd5512f7e2a959fc9de8af7e8aa
Marcel Dan <gigi.dan2011@gmail.com>
2020-04-18 23:55:26 +0200

    adaptado para VSCode y Eclipse

--- SQL/Scripts & Database/RandmonQuerys.sql
+++ SQL/Scripts & Database/RandmonQuerys.sql
@@ -1,14 +1,3 @@
-select idProducto, count(idProducto) from productosusuario natural join ingredientesproducto natural join intoleranciasingrediente where idIntolerancia=1 group by idProducto;
-
-select idProducto, count(idProducto) from productosusuario natural join intoleranciasusuario where idIntolerancia = 1 group by idProducto;
-
-select * from (select idProducto, count(idProducto) from productosusuario natural join ingredientesproducto natural join intoleranciasingrediente where idIntolerancia=1 group by idProducto) t1
-
-natural join
-
-(select idProducto, count(idProducto) from productosusuario natural join intoleranciasusuario where idIntolerancia = 1 group by idProducto) t2;
-
-
 
 select idProducto from productosusuario natural join producto 
 natural join ingredientesproducto natural join ingrediente natural join intoleranciasingrediente natural join intolerancia where idIntolerancia=1  group by idProducto;
@@ -22,11 +11,12 @@ select nombreProducto from producto where producto.idProducto in (select idProdu
 natural join ingredientesproducto natural join ingrediente natural join intoleranciasingrediente natural join intolerancia where idIntolerancia=1);
 
 
-
-
-
-
-
-
 select idProducto,nombreProducto, count(idProducto) from productosusuario natural join producto where productosusuario.idProducto in (select idProducto from productosusuario natural join producto 
 natural join ingredientesproducto natural join ingrediente natural join intoleranciasingrediente natural join intolerancia where idIntolerancia=1) group by IdProductoproducto;
+
+
+
+select nombreProducto from producto where nombreProducto like '%galletas%';
+
+
+select idProducto, nombreProducto from producto;
